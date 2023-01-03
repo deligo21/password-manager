977bc47e68135fcf298a3189cf999e62643b4e5d
deligo21 <arrp1239@gmail.com>
2023-01-03 17:55:15 -0400

    2nd version

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     
@@ -25,24 +26,68 @@ def generate_password():
     password = "".join(password_list)
     password_input.insert(0, password)
     pyperclip.copy(password)
+    
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_information():
-    wb = website_input.get()
+    wb = website_input.get().lower()
     email = email_input.get()
     pswd = password_input.get()
     
+    new_data = {wb:{
+        "email": email,
+        "password": pswd
+    }}
+    
     if len(wb) != 0 and len(email) != 0 and len(pswd) != 0:
         
-        is_ok = messagebox.askokcancel(title=wb, message=f"These are the details entered:\nEmail: {email}\nPassword:{pswd}\nIs it ok to save?")
-        if is_ok:
-        
-            with open("information.txt", "a") as file:
-                file.write(f"{wb} | {email} | {pswd}\n")
-        
+        try:     
+            with open("information.json", "r") as file:
+                #reading old data
+                data = json.load(file)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):       
+            with open("information.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            #Check if the website is in the database
+            if wb in data:
+                update = messagebox.askyesno("Warning", f"There is already a password saved for\nWould you like to overwrite?")
+                if update:
+                    data[wb]["email"] = email
+                    data[wb]["password"] = pswd
+                else:
+                    return
+            
+            #Updating old data with new data
+            data.update(new_data)
+            
+            with open("information.json", "w") as file:
+                #Saving updated data
+                json.dump(data, file, indent=4)
+        finally:
             website_input.delete(0,'end')
             password_input.delete(0,'end')
+        
     else:
         messagebox.showwarning(title="Opps", message="Please don't leave any fields empty!")
+        
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def find_password():
+    wb = website_input.get().lower()
+    try:     
+        with open("information.json", "r") as file:
+            #reading old data
+            data = json.load(file)
+            
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        messagebox.showwarning("Error", "There is no data")
+    
+    else:
+        if wb in data:
+            messagebox.showinfo(wb.title(), f'Email: {data[wb]["email"]}\nPassword: {data[wb]["password"]}')
+        else:
+            messagebox.showwarning("Error", "There is no coincidences")
+        
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -64,8 +109,11 @@ password_txt = Label(text="Password: ", font=("Cascadia", 10, "normal"))
 password_txt.grid(row=3, column=0)
 
 website_input = Entry()
-website_input.grid(row=1, column=1, columnspan=2, sticky=EW)
+website_input.grid(row=1, column=1, sticky=EW)
 website_input.focus()
+
+btn_search = Button(text="Search", command=find_password)
+btn_search.grid(row=1, column=2, sticky=EW)
 
 email_input = Entry()
 email_input.grid(row=2, column=1, columnspan=2, sticky=EW)
